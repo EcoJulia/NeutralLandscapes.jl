437871cbe2a8e4020ee632a4ee32bba5aa78f64d
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-10 09:34:48 -0500

    ❓ add test

---
+++ test/runtests.jl
@@ -0,0 +1,26 @@
+using NeutralLandscapes
+using Test
+
+global anyerrors = false
+
+tests = [
+   "utilities" => "utilities.jl"
+]
+
+for test in tests
+   try
+      include(test.second)
+      println("\033[1m\033[32m✓\033[0m\t$(test.first)")
+   catch e
+      global anyerrors = true
+      println("\033[1m\033[31m×\033[0m\t$(test.first)")
+      println("\033[1m\033[38m→\033[0m\ttest/$(test.second)")
+      showerror(stdout, e, backtrace())
+      println()
+      break
+   end
+end
+
+if anyerrors
+   throw("Tests failed")
+end
