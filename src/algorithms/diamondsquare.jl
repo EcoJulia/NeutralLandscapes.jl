5dab7dcbebc2101d61f1e69239fc09be0550e326
michael catchen <mdcatchen@gmail.com>
2021-02-10 18:31:28 -0500

    constructor for diamond square yada yada

--- src/algorithms/diamondsquare.jl
+++ src/algorithms/diamondsquare.jl
@@ -11,4 +11,17 @@ The result of the diamond-square algorithm is a fractal with dimension D = 2 + H
 
 struct DiamondSquare <: NeutralLandscapeMaker
     H::Float64
+    DiamondSquare(H::T) where {T <: Real} = new(H-floor(H)) # enforce H ∈ [0,1]
+end
+
+"""
+    DiamondSquare()
+
+Creates a `DiamondSquare` that believes in centrism.
+"""
+DiamondSquare() = DiamondSquare(0.5)
+
+function _landscape!(mat, alg::DiamondSquare; kw...) where {IT <: Integer}
+    # the easiest algorithm: do nothing
+    return mat
 end
