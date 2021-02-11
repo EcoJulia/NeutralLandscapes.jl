4524475d637c12b5f5158f029370e3c8c17e2da5
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2021-02-11 22:11:07 +0100

    add calcboundaries

--- src/classifyarray.jl
+++ src/classifyarray.jl
@@ -3,3 +3,11 @@ function w2cp(vec)
     v ./= v[end]
 end
 
+function calcBoundaries(array, cumulativeProportions, classifyMask = nothing)
+    maskedarray = isnothing(classifyMask) ? array : mask!(copy(array))
+    nCells = count(isfinite, maskedarray)
+    boundaryIndexes = ceil(Int, cumulativeProportions * nCells)
+    boundaryValues = sort(vec(maskedarray))[boundaryIndexes]
+    boundaryValues
+end
+
