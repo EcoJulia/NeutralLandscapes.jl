b0b6a6ab945704e2dc51d2dd04ff3b1ee30bd0d1
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-10 19:29:45 -0500

    📖 some docstrings

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,25 @@
 # NeutralLandscapes.jl
 
 A pure Julia port of https://github.com/tretherington/nlmpy
+
+## Landscape models
+
+```@docs
+NeutralLandscapeMaker
+NoGradient
+PlanarGradient
+EdgeGradient
+```
+
+## Landscape generating function
+
+```@docs
+landscape!
+landscape
+```
+
+## Other functions
+
+```@docs
+mask!
+```
--- src/landscape.jl
+++ src/landscape.jl
@@ -1,35 +1,46 @@
+"""
+    _rescale!(mat)
 
-function rescale!(mat)
-  mat .-= NaNMath.minimum(mat)
-  mat ./= NaNMath.maximum(mat)
+Changes the matrix `mat` so that it is between `0` and `1`.
+"""
+function _rescale!(mat)
+    mat .-= NaNMath.minimum(mat)
+    mat ./= NaNMath.maximum(mat)
 end 
 
+"""
+    mask!(array::AbstractArray{<:AbstractFloat}, maskarray::AbstractArray{<:AbstractBool}) 
+
+Modifies `array` so that the positions at which `maskarray` is `false` are
+replaced by `NaN`.
+"""
 function mask!(array::AbstractArray{<:Float64}, maskarray::AbstractArray{<:Bool}) 
+    (size(array) == size(maskarray)) || throw(DimensionMismatch("The dimensions of array, $(size(array)), and maskarray, $(size(maskarray)), must match. "))
     array[.!maskarray] .= NaN
     array
 end
 
 """
-    landscape(alg, s::Tuple{IT,IT}=(10,30)) where {IT <: Integer}
+    landscape(alg, dims::Tuple{Vararg{Int64,2}}; mask=nothing) where {T <: Integer}
 
-Creates a landscape of size `s` following the model defined by `alg`. The
-additional arguments `kw...` are passed to the post-processing function, see the
-documentation of **TODO**. 
+Creates a landscape of size `dims` (a tuple of two integers) following the model
+defined by `alg`. The `mask` argument accepts a matrix of boolean values, and is
+passed to `mask!` if it is not `nothing`. 
 """
-function landscape(alg, size; mask = nothing, kw...) 
+function landscape(alg, dims::Tuple{Vararg{Int64,2}}; mask=nothing) where {T <: Integer}
     ret = Matrix{Float64}(undef, size...)
-    landscape!(ret, alg; mask = mask, kw...)
+    landscape!(ret, alg; mask=mask)
 end
 
 """
     landscape!(mat, alg) where {IT <: Integer}
 
-Fill the matrix `mat` with a landscape of size `s` following the model defined by `alg`. The
-additional arguments `kw...` are passed to the post-processing function, see the
-documentation of **TODO**. 
+Fill the matrix `mat` with a landscape created following the model defined by
+`alg`. The `mask` argument accepts a matrix of boolean values, and is passed to
+`mask!` if it is not `nothing`. 
 """
-function landscape!(mat, alg; mask = nothing, kw...)
-  _landscape!(mat, alg; kw...)
-  isnothing(mask) || mask!(mat, mask)
-  rescale!(mat)
+function landscape!(mat, alg; mask=nothing)
+    _landscape!(mat, alg)
+    isnothing(mask) || mask!(mat, mask)
+    _rescale!(mat)
 end
