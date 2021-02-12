17b47ce7cf9958d22acbf5bb17ead8e2b9433919
michael catchen <mdcatchen@gmail.com>
2021-02-12 16:14:04 -0500

    loop structure for diamond-square

--- src/algorithms/diamondsquare.jl
+++ src/algorithms/diamondsquare.jl
@@ -29,6 +29,49 @@ Creates a `DiamondSquare` that believes in centrism.
 DiamondSquare() = DiamondSquare(0.5)
 
 function _landscape!(mat, alg::DiamondSquare; kw...) where {IT <: Integer}
-    # the easiest algorithm: do nothing
+    # diamond-squares only works on a square lattice of size N x N, 
+    # where N =2^n for some integer n. 
+    @assert isPowerOfTwo(size(mat)[1]) && isPowerOfTwo(size(mat)[2]) 
+    latticeSize::Int = size(mat)[1]
+    numberOfRounds::Int = log2(latticeSize)
+
+    for i in 0:(numberOfRounds-1)    # counting from 0 saves us a headache later
+        subsquareSideLength::Int = 2^(numberOfRounds-i)
+        
+        numberOfSubsquares::Int = latticeSize / subsquareSideLength
+
+        @show subsquareSideLength, numberOfSubsquares
+        # now we want to iterate over all squares of size sideLength x sideLength
+        
+        for x in 0:numberOfSubsquares
+            for y in 0:numberOfSubsquares
+                subsquareCorners = cornerCoordinates(x,y,subsquareSideLength) 
+                square!(mat, alg, subsquareCorners)
+                diamond!(mat, alg, subsquareCorners)
+            end
+        end
+
+   
+    end
+
     return mat
 end
+
+function cornerCoordinates(x::Int, y::Int, sideLength::Int)
+    corners = [(sideLength*x, sideLength*y), (sideLength*(x+1), sideLength*y), (sideLength*x, sideLength*(y+1)), (sideLength*(x+1), sideLength*(y+1))]
+end
+
+function isPowerOfTwo(x::IT) where {IT <: Integer}
+    return log2(x) == floor(log2(x))
+end
+
+function diamond!(mat, alg::DiamondSquare, corners::AbstractVector{Tuple{Int, Int}})
+end
+
+function square!(mat, alg::DiamondSquare, corners::AbstractVector{Tuple{Int, Int}})
+    # compute the center of this square
+    ll,lr,tl,tr = corners
+
+end
+
+
