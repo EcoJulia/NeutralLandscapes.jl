b7cf9074e6be5c3f7af22178bae5531da01af5b4
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2021-02-15 08:43:19 +0100

    inline functions

--- src/classify.jl
+++ src/classify.jl
@@ -1,24 +1,16 @@
-function _w2cp(vec) 
-    v = cumsum(vec)
-    v ./ v[end]
-end
-
-function _calcBoundaries(array, cumulativeProportions, classifyMask = nothing)
-    if isnothing(classifyMask) 
-        quantile(vec(array), cumulativeProportions) 
-    else
-        quantile(array[classifyMask], cumulativeProportions) 
-    end
-end
 
 """
     classify!(array, weights[, classifyMask])
 
 Classify an array into proportions based upon a list of class weights.
 """
-function classify!(array, weights, classifyMask = nothing)
-    cumulativeProportions = _w2cp(weights)
-    boundaryvalues = _calcBoundaries(array, cumulativeProportions, classifyMask)
+function classify!(array, weights, mask = nothing)
+    quantiles = zeros(length(weights))
+    cumsum!(quantiles, weights)
+    quantiles ./= quantiles[end]
+    boundaryvalues = isnothing(mask) ?
+        quantile(filter(isfinite, array), quantiles) :
+        quantile(array[mask .& isfinite.(array)], quantiles)
     for i in eachindex(array)
         array[i] = isnan(array[i]) ? NaN : searchsortedfirst(boundaryvalues, array[i])
     end
