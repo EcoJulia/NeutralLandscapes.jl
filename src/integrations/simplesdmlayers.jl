ec0930d37e9be08c04361f429e3367fe792be1a1
michael <michael@pop-os.localdomain>
2022-09-10 11:13:57 -0400

    :pencil: documenting integration

--- src/integrations/simplesdmlayers.jl
+++ src/integrations/simplesdmlayers.jl
@@ -1,5 +1,12 @@
 @info "Loading NeutralLandscapes support for SimpleSDMLayers.jl..."
 
+"""
+    NeutralLandscapes.mask!(array::AbstractArray{<:Float64}, masklayer::T) where {T<:SimpleSDMLayers.SimpleSDMLayer} 
+
+Masks an `array` by the values of `nothing` in `masklayer`, where `masklayer` is
+a `SimpleSDMLayer`
+
+"""
 function NeutralLandscapes.mask!(array::AbstractArray{<:Float64}, masklayer::T) where {T<:SimpleSDMLayers.SimpleSDMLayer} 
     I = findall(isnothing, masklayer.grid)
     array[I] .= NaN
