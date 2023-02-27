839116c70233684ab909862a7d90262ae4d53615
michael <michael@pop-os.localdomain>
2023-02-27 14:27:36 -0500

    :sparkles: patch maker

--- src/integrations/simplesdmlayers.jl
+++ src/integrations/simplesdmlayers.jl
@@ -7,7 +7,7 @@ Masks an `array` by the values of `nothing` in `masklayer`, where `masklayer` is
 a `SimpleSDMLayer`
 
 """
-function NeutralLandscapes.mask!(array::AbstractArray{<:Float64}, masklayer::T) where {T<:SimpleSDMLayers.SimpleSDMLayer} 
+function NeutralLandscapes.mask!(array::AbstractArray{<:Float64}, masklayer::T) where {T<:SpeciesDistributionToolkit.SimpleSDMLayers.SimpleSDMLayer} 
     I = findall(isnothing, masklayer.grid)
     array[I] .= NaN
     return array
--- test/integrations.jl
+++ test/integrations.jl
@@ -1,11 +1,10 @@
 using NeutralLandscapes
 using Test
-using SimpleSDMLayers
+using SpeciesDistributionToolkit
 
 
 bbox = (left=-83.0, bottom=46.4, right=-55.2, top=63.7)
-temp = convert(Float32, SimpleSDMPredictor(WorldClim, BioClim, 7; bbox...))
-
+temp = SimpleSDMPredictor(RasterData(WorldClim2, AverageTemperature); bbox...) 
 
 mpd = rand(MidpointDisplacement(), size(temp), mask=temp)
 @test findall(isnan, mpd) == findall(isnothing, temp.grid)
