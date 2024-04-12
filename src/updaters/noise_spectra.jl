013afe929a675f9c022072743e6e363ce0cc70f3
michael d. catchen <mdcatchen@gmail.com>
2024-04-12 18:51:56 -0400

    foo? bar?

--- src/updaters/noise_spectra.jl
+++ src/updaters/noise_spectra.jl
@@ -13,21 +13,31 @@ signal(f, n) = [Float32(f(i)) for i in 1:n]
 white_noise(n) = rand(Normal(0,1), 2n) 
 
 n = 300
-noise_mag = 0.05
+noise_mag = 0.02
 
 α = 2.
 
+# Provides a power-law distribution of frequencies
 H = exponential_increase(α, n)
+
+# Get Gaussian noise 
 W = white_noise(n)
 
+
+# Fourier transform to get frequency distribution
+# - H_ft: distribution of frequencies is signal _intentionally designed_ to resemble
+#   power law 
+# - W_ft: Independent draws from Std. Normal (i.e. Brownian motion)
 H_ft, W_ft = fft(H), fft(W)
 noise_spectra = H_ft .* W_ft
 
 # choose real or im here, the only difference is π/2
 noise = real.(ifft(noise_spectra))[1:n] 
 
+# scale noise to mean 0, var 1.
 noise ./= std(noise)
 noise .-= mean(noise)
+
 
 x = signal(x->sin(0.2x)+0.0005x,n) 
 x ./= sum(x)
